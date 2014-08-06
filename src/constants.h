4679aff2c59e78973285b8fa6e00ccd7a16e048c
Travis McLane <tmclane@gmail.com>
2014-08-06 18:41:47 -0500

    add blink code

---
+++ src/constants.h
@@ -0,0 +1,4 @@
+#include <Arduino.h>
+
+const int ledPin =  13;
+int ledState = LOW;
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include <Arduino.h>
 #include "constants.h"
+
+unsigned long last_run;
 
 void setup()
 {
@@ -8,5 +9,12 @@ void setup()
 
 void loop()
 {
+  unsigned long current_time = millis();
+  if(current_time - last_run > 1000) {
+    last_run = current_time;
+    ledState = ledState == HIGH ? LOW : HIGH;
+    digitalWrite(ledPin, ledState);
+    // FIXME: Do work here
+  }
 
 }
