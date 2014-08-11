be9ec651af5616681d23438522de7dce55661f1d
Travis McLane <tmclane@gmail.com>
2014-08-10 19:48:37 -0500

    change bottom undershoot to 1

--- src/globals.cpp
+++ src/globals.cpp
@@ -5,8 +5,8 @@ int ledState = LOW;
 
 // Initial configuration
 int bottom_temp_setting = 65;
-int bottom_temp_undershoot = 1;
-int bottom_temp_overshoot = .5;
+int bottom_temp_undershoot = 0.5;
+int bottom_temp_overshoot = 0.5;
 int bottom_zone_state = 0;
 
 int glycol_temp_setting = 34;
