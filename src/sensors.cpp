ab30ab19ac9d77ce163bcd02ff13247c39fb2927
Travis McLane <tmclane@gmail.com>
2014-09-07 12:32:29 -0500

    wip: averages

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -187,14 +187,6 @@ void update_sensors(int onewire_pin)
   for (int i=0; i<sensor_count; i++){
     int sample = sensors[i].sample;
 
-    if (sample == 3) {
-      sensors[i].fahrenheit_avg = average(sensors[i].f_samples, 3);
-      sensors[i].celsius_avg = average(sensors[i].c_samples, 3);
-
-      sensors[i].sample = -1;
-      sample = 0;
-    }
-
     Serial.print("Processing for sample - #");
     Serial.println(sample);
 
@@ -211,6 +203,14 @@ void update_sensors(int onewire_pin)
     sensors[i].f_samples[sample] = current_f;
     sensors[i].c_samples[sample] = current_c;
 
-    sensors[i].sample++;
+    if (sample == 2) {
+      sensors[i].fahrenheit_avg = average(sensors[i].f_samples, 3);
+      sensors[i].celsius_avg = average(sensors[i].c_samples, 3);
+
+      sensors[i].sample = -1;
+      sample = 0;
+    }
+    else
+      sensors[i].sample++;
   }
 }
