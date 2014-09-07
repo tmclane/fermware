5d5e65d533f60e900f1113b8cbe2beb63b0ac74d
Travis McLane <tmclane@gmail.com>
2014-09-07 10:54:23 -0500

    fixing activation time

--- src/system.cpp
+++ src/system.cpp
@@ -4,6 +4,7 @@
 #include "system.h"
 
 unsigned long system_last_time;
+unsigned long sensor_last_time;
 
 #define ACTIVATION_TIME 10
 #define SENSOR_UPDATE_TIME ACTIVATION_TIME / 2
@@ -36,12 +37,14 @@ float zone_temperature(const byte address[8])
 // Rules are hardcoded for now.
 void maintain_system(unsigned long current_time)
 {
-  if ((current_time - system_last_time) / 1000 > SENSOR_UPDATE_TIME){
+  if ((current_time - sensor_last_time) / 1000 > SENSOR_UPDATE_TIME || current_time == -1){
+    sensor_last_time = current_time;
     Serial.println("Updating sensor values");
     update_sensors(SENSOR_PIN);
   }
 
   if ((current_time - system_last_time) / 1000 > ACTIVATION_TIME || current_time == -1){
+    system_last_time = current_time;
 
     // Maintain Ale Zone (Bottom Chamber)
     float zone_temp = zone_temperature(BOTTOMCHAMBER_ADDR);
@@ -83,7 +86,5 @@ void maintain_system(unsigned long current_time)
       digitalWrite(AIRCON, HIGH);  // Disable cooling
       glycol_state = IDLE;
     }
-
-    system_last_time = current_time;
   }
 }
